8fe92c6f2a6a6abfc16264e7e029508e26b8de78
Ankur <3ankur.v@gmail.com>
2019-11-28 20:53:08 +0000

    added schema changes

--- schema/schema.js
+++ schema/schema.js
@@ -4,9 +4,15 @@ const {GraphQLObjectType,GraphQLString,GraphQLSchema,GraphQLID } = graphql;
 
 
 const books = [
-    {id:"1",name:"The Javascript",genre:"Programming"},
-    {id:"2",name:"5 Points Someone",genre:"Novel"},
-    {id:"3",name:"The Power of Positive Mind",genre:"Motivation"}
+    {id:"1",name:"The Javascript",genre:"Programming",authorId:"1"},
+    {id:"2",name:"5 Points Someone",genre:"Novel",authorId:2},
+    {id:"3",name:"The Power of Positive Mind",genre:"Motivation",authorId:3}
+];
+
+const authors = [
+    {id:"1",name:"Nepolian Hills",age:45},
+    {id:"2",name:"Brendrem NIkolas",age:65},
+    {id:"3",name:"Siaon T",age:41},
 ];
 
  const BookType = new GraphQLObjectType({
@@ -14,7 +20,24 @@ const books = [
     fields:()=>({
         id:{type:GraphQLID},
         name:{type:GraphQLString},
-        genre:{type:GraphQLString}
+        genre:{type:GraphQLString},
+        author:{
+            type:AuthorType,
+            resolve:(parent,args)=>{
+                console.log(parent);
+                return _.find(authors,{id:parent.authorId})
+            }
+        },
+        
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name:"Author",
+    fields:()=>({
+        id:{type:GraphQLID},
+        name:{type:GraphQLString},
+        age:{type:GraphQLString}
     })
 });
 
@@ -28,6 +51,13 @@ const RootQuery = new GraphQLObjectType({
         resolve:(parent,args)=>{
             return  _.find(books, { id: args.id });
         }
+        },
+        author:{
+            type:AuthorType,
+            args:{id:{type:GraphQLID}},
+            resolve:(parent,args)=>{
+                return _.find(authors,{id:args.id})
+            }
         }
         
     }
